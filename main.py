2bc084371187b2297ee2d409675fb432b598954f
AddictionLord <marousiinek@gmail.com>
2021-03-08 14:07:37 +0100

    Added Game class

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 import pygame
 
-WIDTH, HEIGHT = 900, 600
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Snake!")
 
-ICON = pygame.image.load("icon/snake.png")
-pygame.display.set_icon(ICON)
+class Game:
+    WIDTH, HEIGHT = 900, 600
+    WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Snake!")
 
-FPS = 60
+    ICON = pygame.image.load("icon/snake.png")
+    pygame.display.set_icon(ICON)
 
-DARK_BLUE = (0, 0, 75)
+    FPS = 60
+
+    DARK_BLUE = (0, 0, 75)
+
+    def __init__(self):
+        self.main()
+
+    def draw_window(self):
+        Game.WIN.fill(Game.DARK_BLUE)  # insert tuple with RGB values
+        pygame.display.update()
 
 
-def draw_window():
-    WIN.fill(DARK_BLUE)  # insert tuple with RGB values
-    pygame.display.update()
+    def main(self):
+        clock = pygame.time.Clock() 
+        run = True
+        while run:
+            clock.tick(Game.FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
 
+            self.draw_window()
 
-def main():
-    clock = pygame.time.Clock() 
-    run = True
-    while run:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-
-        draw_window()
-
-    pygame.quit()
+        pygame.quit()
 
 if __name__ == "__main__":
-    main()
+    g = Game()
