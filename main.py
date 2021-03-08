b178b4448b2e8f55ed8d0671e4be627ed7b8d6c7
AddictionLord <marousiinek@gmail.com>
2021-03-08 21:47:54 +0100

    Snake class added, main modified

--- main.py
+++ main.py
@@ -1,38 +1,52 @@
 import pygame
+import sys
+from snk import *
 
 
 class Game:
+    pygame.init()
+
+    # Screen
     WIDTH, HEIGHT = 900, 600
     WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    # Title
     pygame.display.set_caption("Snake!")
 
+    # Icon
     ICON = pygame.image.load("icon/snake.png")
     pygame.display.set_icon(ICON)
 
     FPS = 60
-
-    DARK_BLUE = (0, 0, 75)
+    DARK_BLUE = (0, 0, 75) # RGB Values
 
     def __init__(self):
+        self.snake = Snake()
         self.main()
 
-    def draw_window(self):
+    def graphics(self):
         Game.WIN.fill(Game.DARK_BLUE)  # insert tuple with RGB values
+        Game.WIN.blit(self.snake.head, (self.snake.x, self.snake.y))
         pygame.display.update()
 
 
     def main(self):
         clock = pygame.time.Clock() 
         run = True
+
+        # Game loop
         while run:
             clock.tick(Game.FPS)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
 
-            self.draw_window()
+            self.graphics()
 
         pygame.quit()
+        sys.exit()
 
 if __name__ == "__main__":
+    s = Snake()
+    print(s.x)
     g = Game()
---
+++ snk.py
@@ -0,0 +1,28 @@
+import pygame
+
+class Snake:
+    def __init__(self):
+        self.x = 450
+        self.y = 300
+        self.size = 5
+        self.colour = (0, 255, 0) #Green
+        self.head = pygame.Surface((20, 20))
+        self.head.fill(self.colour)
+
+
+    def grow(self):
+        self.size += 1
+
+    def go_up(self):
+        self.y += 1
+
+    def go_down(self):
+        self.y -= 1
+    
+    def go_left(self):
+        self.x += 1
+    
+    def go_right(self):
+        self.x -= 1
+
+
