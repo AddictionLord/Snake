1c686764cf01339eceb2633238e5f95d0b4d0abf
AddictionLord <marousiinek@gmail.com>
2021-03-19 16:18:41 +0100

    Button class created

--- button.py
+++ button.py
@@ -14,29 +14,27 @@ class Button:
         font = pygame.font.SysFont("georgia", self.text_size)
         text_surface = font.render(self.text, 1, colour)
         text_rect = text_surface.get_rect()
-        text_rect.center = (self.x, self.y)
 
-        self.x_border = text_rect[0], text_rect[2]
-        self.y_border = text_rect[1], text_rect[3]
+        width = text_rect[2] - text_rect[0]
+        height = text_rect[3] - text_rect[1]
+        self.x_border = (self.x - width/2, self.x + width/2)
+        self.y_border = (self.y - height/2, self.y + height/2)
 
+        text_rect = text_surface.get_rect(center=(self.x, self.y))
         screen.blit(text_surface, text_rect)
-        pygame.draw.rect(screen, colour, text_rect)
+
         
 
 
     def is_over(self, pos):
         
-        print(pos)
-        print(self.x_border, self.y_border)
         in_x = self.x_border[0] < pos[0] and pos[0] < self.x_border[1]
-        in_y = self.y_border[0] > pos[1] and pos[1] > self.y_border[1] 
+        in_y = self.y_border[0] < pos[1] and pos[1] < self.y_border[1]
 
         if in_x and in_y:
-            print(True)
             return True
 
         else:
-            print(False)
             return False
 
 
